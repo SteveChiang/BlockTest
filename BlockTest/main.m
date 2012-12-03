c98ffc263e1917687468c81bfd228a7322b76a0b
Steve Chiang <csimmaster@gmail.com>
2012-12-03 10:18:42 +0800

    Initial Commit

---
+++ BlockTest/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  BlockTest
+//
+//  Created by Steve on 12/12/3.
+//  Copyright (c) 2012年 Steve. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
